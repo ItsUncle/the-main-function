6f7b8fd5656a7f258e9911d89f95855a518a79d5
ItsUncle <139184678+ItsUncle@users.noreply.github.com>
2023-07-25 00:09:46 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,23 +3,30 @@
 #include <conio.h>
 using namespace std;
 
-int main()
+MainMenu()
 {
-    cout << "\nHOTEL MANAGEMENT APPLICATION"
+     cout << "\nHOTEL MANAGEMENT APPLICATION"
         << "\n---------------------------------"
         << "\nADMIN LOGIN - Press a"
         << "\nGUEST LOGIN - Press g"
         << "\nEXIT PROGRAM - Press ESC\n\n";
-        char option;
-        cin >> option;
+}
+int main() {
+    char option;
+    bool main_menu = false;
     Admin admin1;
-//    Room room1;
-   // admin1.showAllGuests();
+    Guest guest1;
 
-    if (option == 'a')
-    {
-        int choice;
-        while(true) {
+        while (!main_menu) {
+        MainMenu();
+        cin >> option;
+
+        if (option == 'a') {
+            bool login_success = admin1.adminLogin();
+            if (login_success) {
+                int choice;
+                char try_again = 'y';
+               while (try_again == 'y') {
         cout << "\nPlease enter an option"
         << "\n---------------------------------"
             << "\n1.  Show all rooms"
@@ -31,155 +38,120 @@ int main()
             << "\n7.  Search for Guest"
             << "\n8.  Add Guest"
             << "\n9.  Remove Guest"
-            << "\n10. Exit\n\n";
+            << "\n10. Exit\n"
+            << "\nEnter 0 to go to main menu.\n"
+            << ": ";
 
         cin >> choice;
+
         switch (choice)
         {
         case 1:
             admin1.showAllRooms();
+            cout << "press y to go back, press 0 to go to main menu: ";
+            cin >> try_again;
             break;
 
         case 2:
-           {string room_id;
-            cout << "Please enter room id: ";
-            cin >> room_id;
-            admin1.searchForRoom(room_id);}
+            admin1.searchForRoom();
+            cout << "press y to go back, press 0 to go to main menu: ";
+            cin >> try_again;
             break;
 
         case 3:
             admin1.searchAvailableRooms();
+            cout << "press y to go back, press 0 to go to main menu: ";
+            cin >> try_again;
             break;
 
         case 4:
-            {
-              //  Room room_data
-             vector<string> room_data;
-            //Room;
-
-            string id, type, price, status, occupants, capacity;
-            cout << "Please enter the following\n"
-                 << "room id: "; cin>> id;
-            cout<< "\nroom type: "; cin>> type;
-            cout<< "\nroom price: "; cin>> price;
-            cout<< "\nroom status: "; cin>> status;
-            cout<< "\nroom occupants: "; cin>> occupants;
-            cout<< "\nroom capacity: "; cin>> capacity;
-            room_data.push_back(id);
-            room_data.push_back(type);
-            room_data.push_back(price);
-            room_data.push_back(status);
-            room_data.push_back(occupants);
-            room_data.push_back(capacity);
-            admin1.addRoom(room_data);
-            }
-
+            admin1.addRoom();
+            cout << "press y to go back, press 0 to go to main menu: ";
+            cin >> try_again;
             break;
 
         case 5:
-            {
-            string room_id;
-            cout << "Please enter room id: ";
-            cin >> room_id;
-           admin1.removeRoom(room_id);
-           }
+           admin1.removeRoom();
+           cout << "press y to go back, press 0 to go to main menu: ";
+            cin >> try_again;
             break;
 
         case 6:
             admin1.showAllGuests();
+            cout << "press y to go back, press 0 to go to main menu: ";
+            cin >> try_again;
             break;
 
         case 7:
-            {
-            string guest_id;
-            cout << "Please enter guest id: ";
-            cin >> guest_id;
-            admin1.searchForGuest(guest_id);
-            }
+            admin1.searchForGuest();
+            cout << "press y to go back, press 0 to go to main menu: ";
+            cin >> try_again;
             break;
 
         case 8:
-            {
-                vector<string> guest_data;
-                string name, id, contact_info, residency_status, room_id;
-            cout << "Please enter the following\n"
-                 << "guest name: "; cin>> name;
-            cout<< "\nguest id: "; cin>> id;
-            cout<< "\nguest contact: "; cin>> contact_info;
-            cout<< "\nguest residency status: "; cin>> residency_status;
-            cout<< "\nroom id: "; cin>> room_id;
-            guest_data.push_back(name);
-            guest_data.push_back(id);
-            guest_data.push_back(contact_info);
-            guest_data.push_back(residency_status);
-            guest_data.push_back(room_id);
-            admin1.addRoom(guest_data);
-            }
+            admin1.addGuest();
+            cout << "press y to go back, press 0 to go to main menu: ";
+            cin >> try_again;
             break;
 
         case 9:
-            {
-            string guest_id;;
-            cout << "\nPlease enter guest id: ";
-            cin >> guest_id;
-            admin1.removeGuest(guest_id);
-            }
+            admin1.removeGuest();
+            cout << "press y to go back, press 0 to go to main menu: ";
+            cin >> try_again;
             break;
 
         case 10:
             return 0;
 
+        case 0:
+            try_again = 'n';
+            break;
+
         default:
-            cout<< "You entered an invalid option, please try again."<< endl;
+            cout<< "You entered an invalid option, please try again. "<< endl;
+            try_again = 'y';
             break;
         }
-        cin.get();
       }
-    }
 
-    else if (option == 'g')
-    {
-        while(true){
-        int choice;
-        cout << "\nPlease enter an option"
-        << "\n----------------------------------"
-            << "\n1.  Search for available rooms"
-            << "\n2.  Check for personal details"
-            << "\n3.  Leave room"
-            << "\n4.  Exit\n\n";
-        cin >> choice;
-        switch (choice)
-        {
-        case 1:
-            cout << "These are the list of rooms available"
-                << "\n-----------------------------";
-            admin1.searchAvailableRooms();
-            break;
-        case 2:
-            {
-            string guest_id;
-            cout << "Please enter your id: ";
-            cin >> guest_id;
-            admin1.searchForGuest(guest_id);
-            }
-            break;
-        case 3:
-            {
-            string guest_id;;
-            cout << "\nPlease enter your guest id: ";
-            cin >> guest_id;
-            admin1.removeGuest(guest_id);
-            }
-            break;
-        case 4:
-            return 0;
-
-        default:
-            cout<< "You entered an invalid option, please try again."<< endl;
-            break;
-        }
-        cin.get();
-    }
-    return 0;
     }
 }
+
+    else if (option == 'g') {
+            bool login_success = guest1.guestLogin();
+            if (login_success) {
+                int choice;
+                cout << "\nPlease enter an option"
+                     << "\n----------------------------------"
+                     << "\n1.  Search for available rooms"
+                     << "\n2.  Check for personal details"
+                     << "\n3.  Leave room"
+                     << "\n4.  Exit\n\n";
+                cin >> choice;
+                switch (choice) {
+                    case 1:
+                        cout << "These are the list of rooms available"
+                             << "\n-----------------------------";
+                        admin1.searchAvailableRooms();
+                        break;
+                    case 2:
+                        admin1.searchForGuest();
+                        break;
+                    case 3:
+                        admin1.removeGuest();
+                        break;
+                    case 4:
+                        return 0;
+                    default:
+                        cout << "You entered an invalid option, please try again." << endl;
+                        break;
+                }
+            }
+        } else {
+            cout << "Invalid option. Please try again." << endl;
+        }
+    }
+    return 0;
+}
+
+
